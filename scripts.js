01402efb79d5387ef6ac6f751113ef1dd2acd176
sz0320-bit <yknse27@gmail.com>
2022-06-04 11:09:42 -0400

    figured out grid, the hover works but only for the big box

---
+++ scripts.js
@@ -0,0 +1,17 @@
+function gridthingy(){
+
+
+    for(let i = 0; i < 16; i++) {
+        let divbox = document.querySelector("#mainbox");
+        let gridbox = document.createElement('div');
+        gridbox.className = "box"
+        gridbox.style.cssText = "display:inline-block;height:12em;width:12em;border: solid 4px black;";
+        divbox.appendChild(gridbox);
+    }
+}
+
+
+const gridItems = document.querySelectorAll('#grid-container > div');
+gridItems.onmouseover = function(){
+    gridItems.style.cssText = 'background-color: black;';
+}

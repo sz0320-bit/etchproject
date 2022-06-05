3e81696125865f6e8045ea80a4abd2315ce9a380
sz0320-bit <yknse27@gmail.com>
2022-06-04 23:26:10 -0400

    changed css a little, looks better, made new grid and container

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
 function gridthingy(){
 
 
-    for(let i = 0; i < 16; i++) {
+    for(let i = 0; i < (16*16)-16-16; i++) {
         let divbox = document.querySelector("#mainbox");
         let gridbox = document.createElement('div');
         gridbox.className = "box"
-        gridbox.style.cssText = "display:inline-block;height:12em;width:12em;border: solid 4px black;";
+        gridbox.style.cssText = "display:inline-block;height:3.125em;width:3.125em;border: solid 4px white;";
         divbox.appendChild(gridbox);
     }
 }
